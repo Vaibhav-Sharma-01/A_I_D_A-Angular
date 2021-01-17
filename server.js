625ae205a80ed08a285b2e7dbcc62b28c8543460
Vaibhav Sharma <60535703+Vaibhav-Sharma-01@users.noreply.github.com>
2021-01-17 17:26:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require('express');
+const app = express();
+const path = require('path');
 
-app.use(express.static('./dist/UI'))
+app.use(express.static('./dist/UI'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/UI/'}),
-);
+app.get('*', (req, res) => {
+    res.sendFile(`./UI/documentation/index.html`);
+});
 
-app.listen(process.env.PORT || 8080)
+app.listen(process.env.PORT || 8080);
